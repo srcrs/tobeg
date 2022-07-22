26a6804093aa0112d9d3ddf94eb853ead6a1ebab
asksowhat <srcrs@foxmail.com>
2022-07-22 20:59:27 +0200

    为静态代码、模版引擎配置路由

--- initialize/router.go
+++ initialize/router.go
@@ -12,8 +12,13 @@ func Routers() *gin.Engine {
 	// 解决跨域
 	Router.Use(middlerwares.Cors())
 
-	ApiGroup := Router.Group("v1")
-	router.InitPayRouter(ApiGroup)
+	// 加载tmplates目录下模版文件
+	Router.LoadHTMLGlob("./templates/*")
 
+	Router.Static("/public", "./public")
+
+	ApiGroup := Router.Group("v1")
+	router.InitAlipayRouter(ApiGroup)
+	router.InitTmplRouter(Router.Group(""))
 	return Router
 }
